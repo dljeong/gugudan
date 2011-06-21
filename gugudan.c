287b671426daafeb9721ce74a548d39cd7be5875
dljeong <jdl123123@naver.com>
2011-06-21 01:33:35 -0700

    Program 1, initial program

--- gugudan.c
+++ gugudan.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 
-void print_7_n(int n) {
-	printf("7 * %d = %d\n", n, 7*n);
-}
-
 int main(int argc, char *argv[]) {
-
-	int i, n;
-
-	scanf("%d", &n);
-
-	print_7_n(n);
-
-/*	for(i=1;i<10;i++) {
-		print_7_n(i);
-	}*/
 
 	return 0;
 }
